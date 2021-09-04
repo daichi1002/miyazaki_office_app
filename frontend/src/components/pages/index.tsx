f83467b419c28b8bba2d2d2c0b727ecd5b304e4d
noriaki.kamitake <noriaki.kamitake@vitalize.co.jp>
2021-09-04 10:51:02 +0900

    add: README dir

---
+++ frontend/src/components/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import GenericTemplate from "../templates/GenericTemplates";
+const HomePage: React.FC = () => {
+  return (
+    <GenericTemplate title="トップページ">
+      <>トップページ内容</>
+    </GenericTemplate>
+  );
+};
+
+export default HomePage;
