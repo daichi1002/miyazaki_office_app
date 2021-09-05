1c4bbb99a1bba13512d1f9b1ebe9910be407433e
noriaki.kamitake <noriaki.kamitake@vitalize.co.jp>
2021-09-05 22:22:44 +0900

    practise useEffect

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import GenericTemplate from '../components/templates/GenericTemplates'
 const HomePage: React.FC = () => {
   const [count, setCount] = React.useState(0)
+
+  React.useEffect(() => {
+    document.title = `${count}回クリックされました`
+    console.log(`再レンダーされました`)
+  }, [])
+
   return (
     <GenericTemplate title="トップページ">
       <>トップページ内容</>
