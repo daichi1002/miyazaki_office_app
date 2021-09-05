8aefc5f04718d5c30be2fe5e9e79e6ba658cacb9
noriaki.kamitake <noriaki.kamitake@vitalize.co.jp>
2021-09-05 19:04:23 +0900

    practise useState

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import GenericTemplate from '../components/templates/GenericTemplates';
+import React from 'react'
+import GenericTemplate from '../components/templates/GenericTemplates'
 const HomePage: React.FC = () => {
+  const [count, setCount] = React.useState(0)
   return (
     <GenericTemplate title="トップページ">
       <>トップページ内容</>
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>++</button>
     </GenericTemplate>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
