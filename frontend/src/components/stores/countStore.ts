204e018331cd69bc579c3ec193257aa5a6d4c7df
noriaki.kamitake <noriaki.kamitake@vitalize.co.jp>
2021-09-06 20:22:22 +0900

    add: first-view

--- frontend/src/components/stores/countStore.ts
+++ frontend/src/components/stores/countStore.ts
@@ -1,24 +1,24 @@
-  import { observable,computed, action, makeObservable } from 'mobx';
+import { observable, computed, action, makeObservable } from 'mobx'
 
-    export class CountStore {
-    count = 1
+export class CountStore {
+  count = 1
 
-    get getDoubleCount() {
-      return this.count * 2;
-    }
-
-    setIncrementCount() {
-      console.log(this.count++)
-    }
-    constructor() {
-      makeObservable(this,{
-      count:  observable,
-      getDoubleCount:  computed,
-      setIncrementCount:  action,
-      })
-    }
+  get getDoubleCount() {
+    return this.count * 2
   }
 
+  setIncrementCount() {
+    this.count++
+  }
 
-
-   
+  setDecrimentCount() {
+    this.count--
+  }
+  constructor() {
+    makeObservable(this, {
+      count: observable,
+      getDoubleCount: computed,
+      setIncrementCount: action,
+    })
+  }
+}
---
+++ frontend/src/pages/inventory.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GenericTemplate from '../components/templates/GenericTemplates'
+
+const ProductPage: React.FC = () => {
+  return (
+    <GenericTemplate title="在庫ページ">
+      <>在庫ページ</>
+    </GenericTemplate>
+  )
+}
+
+export default ProductPage
