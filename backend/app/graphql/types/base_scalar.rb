07c6197b81c3798eaed5f570bae9289f415665b1
daichinagano <d.nagano1002@gmail.com>
2021-11-14 10:41:36 +0900

    update

--- backend/app/graphql/types/base_scalar.rb
+++ backend/app/graphql/types/base_scalar.rb
@@ -1,4 +1,4 @@
   class Types::DateTime < GraphQL::Schema::Scalar
-      coerce_input ->(value, _ctx) { Time.zone.parse(value) }
+      coerce_input ->(value) { Time.zone.parse(value) }
       coerce_result ->(value, _ctx) { value.utc.strftime("%Y/%m/%d") }
   end
