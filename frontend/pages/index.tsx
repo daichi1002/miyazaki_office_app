8389f72de4666c82454e27490a598d3f2dd1f751
daichinagano <d.nagano1002@gmail.com>
2021-11-17 09:31:08 +0900

    update

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,60 +1,40 @@
-import React from 'react'
-import axios from 'axios'
-import BodyCard from '../components/molecules/BodyCard'
-import Box from '@material-ui/core/Box'
+import React, { useState } from 'react'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
-import { makeStyles } from '@material-ui/core'
-import { Props } from '../components/molecules/BodyCard'
+import ProductInput from '../components/organisms/ProductInput'
+import ProductTable from '../components/organisms/ProductTable'
+import { History, HistoryDetail } from '../types'
 
-const HomePage: React.FC = () => {
-  const [posts, setPosts] = React.useState([])
-  const cardContent = {
-    avatarUrl: 'https://joeschmoe.io/api/v1/random',
-    imageUrl: 'https://picsum.photos/150',
-  }
+const ProductPage = () => {
+  const [subtotal, setSubtotal] = useState<number>(0)
 
-  React.useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then((res) => {
-      setPosts(res.data)
-    })
-  }, [])
-
-  const getCardContent = (getObj: Props) => {
-    const bodyCardContent = { ...getObj, ...cardContent }
-    return (
-      <Grid item xs={12} sm={4} key={getObj.id}>
-        <BodyCard {...bodyCardContent} />
-      </Grid>
-    )
-  }
-
-  const useStyles = makeStyles((theme) => ({
-    container: {
-      paddingTop: theme.spacing(4),
-      paddingBottom: theme.spacing(4),
-    },
-    paper: {
-      padding: theme.spacing(2),
-      display: 'flex',
-      overflow: 'auto',
-      flexDirection: 'column',
-    },
-    fixedHeight: {
-      height: 300,
-    },
-  }))
-  const classes = useStyles()
+  const [history, setHistory] = useState<History>({ userId: 0, title: '', purchaseAt: '' })
+  const [historyDetail, setHistoryDetail] = useState<HistoryDetail[]>([])
   return (
-    <Container maxWidth="lg" className={classes.container}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          {posts.slice(0, 3).map((contentObj) => getCardContent(contentObj))}
+    <Container maxWidth="lg">
+      <h1>購入登録</h1>
+      <Grid container spacing={10}>
+        <Grid item xs={12} md={6} lg={6}>
+          <ProductInput
+            subtotal={subtotal}
+            setSubtotal={setSubtotal}
+            setHistoryDetail={setHistoryDetail}
+            historyDetail={historyDetail}
+            setHistory={setHistory}
+          />
+        </Grid>
+        <Grid item xs={12} md={6} lg={6}>
+          <ProductTable
+            subtotal={subtotal}
+            setSubtotal={setSubtotal}
+            setHistoryDetail={setHistoryDetail}
+            historyDetail={historyDetail}
+            history={history}
+          />
         </Grid>
       </Grid>
-      <Box pt={4}></Box>
     </Container>
   )
 }
 
-export default HomePage
+export default ProductPage
